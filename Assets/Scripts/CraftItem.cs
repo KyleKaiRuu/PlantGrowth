0f76a010af849c0d5ceb103634cd80e05f4a5096
Kyle Fowler <kylespell101@gmail.com>
2020-06-02 16:29:00 -0700

    Inventory and crafting done

---
+++ Assets/Scripts/CraftItem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftItem : MonoBehaviour
+{
+    public CraftingRecipe recipeToCraft;
+    public InventoryManager inventoryManager;
+
+    public bool canCraft;
+
+    int itemsHave;
+    int quantitiesHave;
+
+    public void Craft()
+    {
+        if (CheckIfCanCraft())
+        {
+            inventoryManager.AddItem(recipeToCraft.itemToCraft, 1);
+            for (int i = 0; i < recipeToCraft.ingredients.Count; i++)
+            {
+                inventoryManager.RemoveItem(recipeToCraft.ingredients[i].itemNeeded, recipeToCraft.ingredients[i].quantityNeeded);
+            }
+        }
+    }
+
+    bool CheckIfCanCraft()
+    {
+        bool itemChecked = false;
+        for (int i = 0; i < recipeToCraft.ingredients.Count; i++)
+        {
+            for (int j = 0; j < inventoryManager.inventory.Count; j++)
+            {
+                if (recipeToCraft.ingredients[i].itemNeeded.itemId == inventoryManager.inventory[j].itemId)
+                {
+                    itemsHave++;
+                    if (inventoryManager.inventory[j].quantity >= recipeToCraft.ingredients[i].quantityNeeded && !itemChecked)
+                    {
+                        quantitiesHave++;
+                        itemChecked = true;
+                    }
+                }
+            }
+            itemChecked = false;
+        }
+
+        if (itemsHave == recipeToCraft.ingredients.Count && quantitiesHave == recipeToCraft.ingredients.Count)
+        {
+            quantitiesHave = 0;
+            itemsHave = 0;
+            return true;
+        }
+        else
+        {
+            quantitiesHave = 0;
+            itemsHave = 0;
+            return false;
+        }
+    }
+}
--- Assets/Scripts/CraftingRecipe.cs
+++ Assets/Scripts/CraftingRecipe.cs
@@ -14,6 +14,6 @@ public class CraftingRecipe : MonoBehaviour
 
     public List<Ingredients> ingredients;
 
+    //This would always be the itemData script attached to this same gameobject
     public ItemData itemToCraft;
-
 }
